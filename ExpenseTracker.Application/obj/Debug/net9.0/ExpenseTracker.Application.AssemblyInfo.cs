41a8a8a12b3ffc279f3effad66f7fd465431099c
RoMaSsHoW <fantomfantomovich0@gmail.com>
2025-10-29 14:51:10 +0500

    configuring User

--- ExpenseTracker.Application/obj/Debug/net9.0/ExpenseTracker.Application.AssemblyInfo.cs
+++ ExpenseTracker.Application/obj/Debug/net9.0/ExpenseTracker.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12056df5aa9f1ef859a79dbed9213e70d29bfb43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa3d7a8acf04f618861ab50dba5dd9da9f88fcfd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ExpenseTracker.Domain/AccountAggregate/User.cs
+++ ExpenseTracker.Domain/AccountAggregate/User.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using ExpenseTracker.Domain.AccountAggregate.ValueObjects;
 using ExpenseTracker.Domain.SeedWork;
 
@@ -5,12 +6,107 @@ namespace ExpenseTracker.Domain.AccountAggregate;
 
 public class User : Entity
 {
+    protected User(
+        string firstName,
+        string lastName,
+        string email,
+        string password,
+        int roleId,
+        string refreshToken,
+        DateTime refreshTokenExpiryDate)
+    {
+        FirstName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(firstName.Trim().ToLower());
+        LastName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(lastName.Trim().ToLower());
+        Email = new Email(email);
+        EmailIsConfirmed = false;
+        Password = new Password(password);
+        Role = Enumeration.FromId<Role>(roleId);
+        RefreshToken = refreshToken;
+        RefreshTokenExpiryDate =  refreshTokenExpiryDate;
+        CreatedAt = DateTime.UtcNow;
+    }
+    
     public string FirstName { get; private set; }
     public string LastName { get; private set; }
     public Email Email { get; private set; }
-    public string Password { get; private set; }
+    public bool EmailIsConfirmed { get; private set; }
+    public Password Password { get; private set; }
     public Role Role { get; private set; }
     public string RefreshToken { get; private set; }
     public DateTime RefreshTokenExpiryDate { get; private set; }
     public DateTime CreatedAt { get; private set; }
+
+    public static User Registration(
+        string firstName,
+        string lastName,
+        string email,
+        string password,
+        int roleId,
+        string refreshToken,
+        DateTime refreshTokenExpiryDate)
+    {
+        if (string.IsNullOrWhiteSpace(firstName))
+            throw new ArgumentException("First name cannot be null or whitespace.", nameof(firstName));
+        if (string.IsNullOrWhiteSpace(lastName))
+            throw new ArgumentException("Last name cannot be null or whitespace.", nameof(lastName));
+        if (string.IsNullOrWhiteSpace(refreshToken))
+            throw new ArgumentException("Refresh token cannot be null or whitespace.", nameof(refreshToken));
+        if (refreshTokenExpiryDate < DateTime.UtcNow)
+            throw new ArgumentException("Refresh token has already expired.");
+        
+        return new User(
+            firstName, 
+            lastName, 
+            email,
+            password,
+            roleId,
+            refreshToken,
+            refreshTokenExpiryDate);
+    }
+    
+    public bool Verify(string password)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, Password.PasswordHash);
+    }
+    
+    public void ChangeRefreshToken(
+        string refreshToken,
+        DateTime refreshTokenExpiryDate)
+    {
+        if (string.IsNullOrWhiteSpace(refreshToken))
+            throw new ArgumentException("RefreshToken cannot be null or whitespace.");
+        if (RefreshToken ==  refreshToken)
+            throw new ArgumentException("RefreshToken has already been changed.");
+        if (refreshTokenExpiryDate < DateTime.UtcNow)
+            throw new ArgumentException("Refresh token has already expired.");
+        
+        RefreshToken = refreshToken;
+        RefreshTokenExpiryDate = refreshTokenExpiryDate;
+    }
+
+    public void ChangeProfile(
+        string firstName,
+        string lastName,
+        string email)
+    {
+        if (string.IsNullOrWhiteSpace(firstName))
+            throw new ArgumentException("First name cannot be null or whitespace.", nameof(firstName));
+        if (string.IsNullOrWhiteSpace(lastName))
+            throw new ArgumentException("Last name cannot be null or whitespace.", nameof(lastName));
+
+        FirstName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(firstName.Trim().ToLower());
+        LastName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(lastName.Trim().ToLower());
+        Email = new Email(email);
+    }
+
+    public void ChangePassword(string oldPassword, string newPassword)
+    {
+        if (string.IsNullOrWhiteSpace(oldPassword))
+            throw new ArgumentException("Old password cannot be null or whitespace.", nameof(oldPassword));
+
+        if (Verify(oldPassword))
+        {
+            Password = new Password(newPassword);
+        }
+    }
 }
